08450f6a6e106cc08aeaba4294f6a198db3d2e29
ArcxExe <net.c0reunity@gmail.com>
2026-01-16 19:39:20 +0400

    fix: При запросе данных , запрос уходит в рекурсию

--- src/main/java/com/example/finance/model/Category.java
+++ src/main/java/com/example/finance/model/Category.java
@@ -1,18 +1,20 @@
 package com.example.finance.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.math.BigDecimal;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "category")
 public class Category {
@@ -22,7 +24,8 @@ public class Category {
   private Long id;
   private String name;
   private BigDecimal limits;
-  @OneToMany(mappedBy = "category")
+  @OneToMany(mappedBy = "category" , cascade =  CascadeType.ALL)
+  @JsonIgnore
   private List<Transaction> transactions;
 
   public Category() {
@@ -31,6 +34,38 @@ public class Category {
   public Category(String name, BigDecimal limits) {
     this.name = name;
     this.limits = limits;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public BigDecimal getLimits() {
+    return limits;
+  }
+
+  public void setLimits(BigDecimal limits) {
+    this.limits = limits;
+  }
+
+  public List<Transaction> getTransactions() {
+    return transactions;
+  }
+
+  public void setTransactions(List<Transaction> transactions) {
+    this.transactions = transactions;
   }
 
 }
